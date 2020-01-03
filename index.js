f282b5f2e821f8de9d1508de20bc5e8d92bb1983
adriansalvatori <adriansalvatori@gmail.com>
2020-01-03 06:36:23 -0500

    Basic Structure

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './resources/assets/styles/main.scss'
+import App from './resources/views/layouts/app.js'
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ resources/views/layouts/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <div className="hero is-fullheight is-primary">
+                    <div className="hero-body">
+                        .<div class="container">
+                        <h1 className="title is-1">This is Parda</h1>
+                        <p>A Parcel | React starter boilerplate by Adrián Salvatori.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default App;
